140405d559bfc496f03320897cbb5ef3f7f4728b
hp <b02902129@ntu.edu.tw>
2016-12-05 20:47:43 +0800

    branch test

---
+++ s.js
@@ -0,0 +1,16 @@
+a=function(x){
+    
+    
+    
+    b=function(y)
+    {
+        return x+y; 
+    }
+    if(typeof y==="undefined")
+        {
+            return x;
+        }
+        else{
+            return b;
+        }
+}
